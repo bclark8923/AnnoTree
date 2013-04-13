6b42f6dc2876732e800186330d99e5a7e25d7e47
Brian <bclark8923@gmail.com>
2013-04-13 17:20:12 -0400

    Draggable
    Draggable button and open vs. closed states of anno tree

---
+++ AnnoTree Viewer/AnnoTree Viewer/AnnoTreeUserLaunchViewController.h
@@ -0,0 +1,13 @@
+//
+//  AnnoTreeUserLaunchViewController.h
+//  AnnoTree Viewer
+//
+//  Created by Brian Clark on 4/11/13.
+//  Copyright (c) 2013 AnnoTree. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AnnoTreeUserLaunchViewController : UIViewController
+
+@end
---
+++ AnnoTree Viewer/AnnoTree Viewer/AnnoTreeUserLaunchViewController.m
@@ -0,0 +1,38 @@
+//
+//  AnnoTreeUserLaunchViewController.m
+//  AnnoTree Viewer
+//
+//  Created by Brian Clark on 4/11/13.
+//  Copyright (c) 2013 AnnoTree. All rights reserved.
+//
+
+#import "AnnoTreeUserLaunchViewController.h"
+
+@interface AnnoTreeUserLaunchViewController ()
+
+@end
+
+@implementation AnnoTreeUserLaunchViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

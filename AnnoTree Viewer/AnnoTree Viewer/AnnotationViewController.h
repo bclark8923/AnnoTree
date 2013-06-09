272f73634021de520748c11ec5a53b398b108ec9
Brian <bclark8923@gmail.com>
2013-06-09 15:23:24 -0400

    Drawing and placing text
    Can now draw and switch to text, place text effectively.
    
    Currently screenshots across uikit/opengles broken

---
+++ AnnoTree Viewer/AnnoTree Viewer/AnnotationViewController.h
@@ -0,0 +1,13 @@
+//
+//  AnnotationViewController.h
+//  AnnoTree Viewer
+//
+//  Created by Brian Clark on 6/7/13.
+//  Copyright (c) 2013 AnnoTree. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AnnotationViewController : UIViewController
+
+@end
---
+++ AnnoTree Viewer/AnnoTree Viewer/AnnotationViewController.m
@@ -0,0 +1,38 @@
+//
+//  AnnotationViewController.m
+//  AnnoTree Viewer
+//
+//  Created by Brian Clark on 6/7/13.
+//  Copyright (c) 2013 AnnoTree. All rights reserved.
+//
+
+#import "AnnotationViewController.h"
+
+@interface AnnotationViewController ()
+
+@end
+
+@implementation AnnotationViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

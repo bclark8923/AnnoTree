1178114b46a6a5d95f38b5d8dc2ad6082c29ee45
suicidejack <mattprice11@gmail.com>
2013-06-12 13:43:52 -0400

    set up db install files to work on my local

---
+++ Database/install_matt_local.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+from os import system
+DEBUG = True
+
+config = open("config/install_matt_local.config").read(-1)
+components = open("config/components.config").read(-1)
+
+username = "annotree"
+password = "ann0tr33s"
+
+for line in config.split("\n"):
+  parts = line.split("=")
+  if len(parts) == 2:
+    key = parts[0]
+    value = parts[1]
+    if key == "username": username = value
+    if key == "password": password = value
+
+
+system('echo "Creating Database" | cowsay')
+line = components.split("\n")[0]
+command = "mysql -u " + username + " --password=" + password + " < " + line
+system(command)
+
+system('echo "Creating Database Tables" | cowsay')
+for line in components.split("\n")[1:]:
+  if len(line) < 1 or line[0] == "#":
+    continue
+  command = "mysql -u " + username + " --password=" + password + " annotree < " + line
+  system(command)
+  if DEBUG:
+    print 'Running: ' + command  

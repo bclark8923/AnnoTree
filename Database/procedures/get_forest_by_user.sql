21794808e36809818e233a2d6386d9c49bb08207
unrealdps <max5@umbc.edu>
2013-06-16 18:37:52 -0400

    Changed in supquery to use join.

--- Database/procedures/get_forest_by_user.sql
+++ Database/procedures/get_forest_by_user.sql
@@ -3,19 +3,18 @@
 -- Note: Returns a list of forest associated with a user's id
 -- --------------------------------------------------------------------------------
 use annotree;
-drop function IF EXISTS `get_user`;
+drop procedure IF EXISTS `get_forest_by_user`;
 DELIMITER $$
 
 CREATE PROCEDURE `get_forest_by_user`(
   in u INT)
 BEGIN
-DECLARE forest_data varchar(1024) default '0';
-select id, name, description, created_at
+select forest.id, forest.name, forest.description, forest.created_at
             from forest
-            where id in (
-                select forest_id
-                from user_forest
-                where user_id = u
-            );
+            join
+                user_forest
+                    on user_forest.forest_id = forest.id  
+                    and user_forest.user_id = u;
 END $$
 delimiter ; $$
+call get_forest_by_user(1);

cc57bee25d875510c770321640b23b2967fd550f
unrealdps <max5@umbc.edu>
2013-06-18 03:20:16 -0400

    Added delte_tree.sql Moved get_trees_by_user_by_forest to correct folder

---
+++ Database/procedures/delete_tree.sql
@@ -0,0 +1,27 @@
+-- --------------------------------------------------------------------------------
+-- delete_tree
+-- returns 0 - success
+-- --------------------------------------------------------------------------------
+use annotree;
+drop  procedure IF EXISTS `delete_tree`;
+DELIMITER $$
+
+
+CREATE Procedure `delete_tree`(
+  in user INT,
+  in tree_id INT
+  )
+BEGIN
+IF (select id from user where id = user and active = true) then
+SET FOREIGN_KEY_CHECKS=0;
+delete t, tu 
+        from tree as t 
+        inner join user_tree as tu on
+            tu.tree_id = t.id
+    where
+        t.id = tree_id and
+        tu.user_id = user;
+SET FOREIGN_KEY_CHECKS=1;
+END IF;
+END $$
+delimiter ; $$

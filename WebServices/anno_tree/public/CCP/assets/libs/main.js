d510b6007f7561294526ecce5a4198de07989bdc
Brian <bclark8923@gmail.com>
2013-06-18 01:54:40 -0400

    CCP Move and early implementation of services

--- WebServices/anno_tree/public/CCP/assets/libs/main.js
+++ WebServices/anno_tree/public/CCP/assets/libs/main.js
@@ -16,18 +16,18 @@ Gumby.oldie(function() {
 // Document ready
 $(document).ready(function() {
   var width = 80;
-  settingsPane = new SlidingPane({
+  /*settingsPane = new SlidingPane({
     id: 'mobileOptions',
     targetId: 'wrapper',
     side: 'right',
     width: 240,
     duration: 0.75,
     timingFunction: 'ease',
-    shadowStyle: '0px 0px 0px #000' /* or 'none' or ... */
+    shadowStyle: '0px 0px 0px #000'
   });
 
   $("#paneToggle").click(function() {
     settingsPane.toggle()
-  });
+  });*/
 });
 

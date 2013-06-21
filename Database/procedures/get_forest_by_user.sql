a84e413c36f8e39a85a664f894d91edb2286228f
Lord of the Servers <lordoftheservers@silith.io>
2013-06-21 00:04:36 -0400

    added a select statement to return the names of the columns

--- Database/procedures/get_forest_by_user.sql
+++ Database/procedures/get_forest_by_user.sql
@@ -9,11 +9,14 @@ DELIMITER $$
 CREATE PROCEDURE `get_forest_by_user`(
   in u INT)
 BEGIN
+select 'id', 'name', 'description', 'created_at'
+union
 select forest.id, forest.name, forest.description, forest.created_at
             from forest
             join
                 user_forest
                     on user_forest.forest_id = forest.id  
                     and user_forest.user_id = u;
+set @id = LAST_INSERT_ID();
 END $$
 delimiter ; $$

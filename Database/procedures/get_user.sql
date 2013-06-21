bdb55370aacdcc4cb0326ab96c9a20fcd73987dd
michael max <mmax@envy>
2013-06-21 00:49:41 -0400

    Updated get_user to be procedure not function

--- Database/procedures/get_user.sql
+++ Database/procedures/get_user.sql
@@ -3,24 +3,17 @@
 -- Note: You need to salt the password in perl.
 -- --------------------------------------------------------------------------------
 use annotree;
-drop function IF EXISTS `get_user`;
+drop procedure IF EXISTS `get_user`;
 DELIMITER $$
 
-CREATE FUNCTION `get_user`(
+CREATE procedure `get_user`(
   e VARCHAR(255))
-returns VARCHAR(1024)
 BEGIN
 DECLARE user_data varchar(1024) default '0';
-SET @user_data = (select concat(id, ', ',
-                                      password, ', ', 
-                                      first_name, ', ',
-                                      last_name, ', ',
-                                      email, ', ',
-                                      IFNULL(lang, ''), ', ',
-                                      IFNULL(time_zone, ''), ', ',
-                                      IFNULL(profile_image_path, ''))
+select 'id', 'password', 'first_name',  'last_name', 'email', 'lang', 'time_zone', 'profile_image_path'
+union
+select id, password, first_name,  last_name, email, lang, time_zone, profile_image_path
             from user 
-            where email = e);
-return @user_data;
+            where email = e;
 END $$
 delimiter ; $$

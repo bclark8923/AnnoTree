08bd57f9709eae996475b3d1b45c2e46eec56403
Michael Max <mmax@potomacfusion.com>
2013-06-22 15:12:56 -0400

    Added get_branches and get_leafs

---
+++ Database/procedures/get_branches.sql
@@ -0,0 +1,23 @@
+-- --------------------------------------------------------------------------------
+-- get_branches
+-- Note: Returns a list of forest associated with a user's id
+-- --------------------------------------------------------------------------------
+use annotree;
+drop  procedure IF EXISTS `get_branches`;
+DELIMITER $$
+
+
+CREATE Procedure `get_branches`(
+  in user INT,
+  in tree INT
+  )
+BEGIN
+select 'id', 'name', 'description', 'tree_id', 'created_at' union
+select b.id, b.name, b.description, b.tree_idd, b.created_at 
+from branch as b 
+    join user_branch ub on
+        ub.branch_id = branch.id and
+        user = ub.user_id and
+        b.tree_id = tree;
+END $$ 
+DELIMITER ; $$
---
+++ Database/procedures/get_leafs.sql
@@ -0,0 +1,23 @@
+-- --------------------------------------------------------------------------------
+-- get_leafs
+-- Note: Returns a list of forest associated with a user's id
+-- --------------------------------------------------------------------------------
+use annotree;
+drop  procedure IF EXISTS `get_leafs`;
+DELIMITER $$
+
+
+CREATE Procedure `get_leafs`(
+  in user INT,
+  in branch INT
+  )
+BEGIN
+select 'id', 'name', 'comment', 'owner_user_id', 'assignee_user_id', 'branch_id', 'created_at' union
+select l.id, l.name, l.comment, l.owner_user_id, l.assignee_user_id, l.branch_id, l.created_at 
+from leaf as l 
+    join user_leaf ul on
+        ul.leaf_id = leaf.id and
+        user = ul.user_id and
+        l.branch_id = branch;
+END $$ 
+DELIMITER ; $$

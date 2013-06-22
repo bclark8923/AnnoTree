4759da179c0b97f0a8e13348fcfea0ef129c6843
Lord of the Servers <lordoftheservers@silith.io>
2013-06-21 21:03:32 -0400

    changed the procedure get_user to return all user variables

--- Database/procedures/get_user.sql
+++ Database/procedures/get_user.sql
@@ -10,9 +10,9 @@ CREATE procedure `get_user`(
   e VARCHAR(255))
 BEGIN
 DECLARE user_data varchar(1024) default '0';
-select 'id', 'password', 'first_name',  'last_name', 'email', 'lang', 'time_zone', 'profile_image_path'
+select 'id', 'password', 'first_name',  'last_name', 'email', 'lang', 'time_zone', 'profile_image_path', 'created_at', 'active'
 union
-select id, password, first_name,  last_name, email, lang, time_zone, profile_image_path
+select id, password, first_name,  last_name, email, lang, time_zone, profile_image_path, created_at, active
             from user 
             where email = e;
 END $$

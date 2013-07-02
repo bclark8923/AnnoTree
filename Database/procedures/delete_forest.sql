401badbcc98240025292a76497911a709cd6cfb1
michael max <mmax@envy>
2013-07-02 04:49:48 -0400

    Updated delte forest to delete all leafs and annotations

--- Database/procedures/delete_forest.sql
+++ Database/procedures/delete_forest.sql
@@ -15,7 +15,7 @@ BEGIN
 IF (select id from user where id = user) then
   SET FOREIGN_KEY_CHECKS=0;
   -- TODO: leafs
-  delete f, uf, t, ut, b, ub 
+  delete f, uf, t, ut, b, ub, l, ul a 
           from forest as f 
            left join user_forest as uf on
               uf.forest_id = f.id
@@ -26,7 +26,13 @@ IF (select id from user where id = user) then
            left join branch as b on
               b.tree_id = t.id
            left join user_branch as ub on
-              ub.branch_id = b.id            
+              ub.branch_id = b.id
+           left join leaf as l on
+              l.branch_id = b.id
+           left join user_leaf as ul on
+              l.id = ul.leaf_id
+           left join annotation as a on
+              a.leaf_id = l.id            
       where
           f.id = forest_id and
           uf.user_id = user;

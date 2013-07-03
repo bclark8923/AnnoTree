3a3041361792369f9a4a031df54d2f173ee01cd3
suicidejack <mattprice11@gmail.com>
2013-07-03 01:47:06 -0400

    edited the procedure to make sure that the requesting user has permissions to add the user to the tree

--- Database/procedures/add_user_to_tree.sql
+++ Database/procedures/add_user_to_tree.sql
@@ -1,6 +1,7 @@
 -- --------------------------------------------------------------------------------
 -- add_user_to_tree
--- returns ??
+-- On success - returns nothing
+-- On failure - returns 1 (requsting user doesn't have permissions on that tree)
 -- --------------------------------------------------------------------------------
 use annotree;
 drop  procedure IF EXISTS `add_user_to_tree`;
@@ -8,22 +9,27 @@ DELIMITER $$
 
 
 CREATE Procedure `add_user_to_tree`(
-  in tree_id int,
-  in user_id int
-  )
+    in treeid int,
+    in userToAdd int,
+    in requestingUser INT
+)
 BEGIN
+    IF (select id from user_tree where tree_id = treeid and user_id = requestingUser) THEN
 START TRANSACTION;
-insert into user_tree (user_id, tree_id) values (user_id, tree_id);
-insert into user_forest(user_id, forest_id) 
-  select user_id, tree.forest_id from tree where tree.id = tree_id;
-insert into user_branch(user_id, branch_id) 
-  select user_id, branch.id from branch where branch.tree_id = tree_id;
-insert into user_leaf(user_id, leaf_id)
-  select user_id, leaf.id from leaf 
-    inner join branch as b
-      on b.tree_id = tree_id and
-      leaf.branch_id = b.id
-    where leaf.branch_id = b.id;
-Commit;
+    insert into user_tree (user_id, tree_id) values (userToAdd, treeid);
+    insert into user_forest(user_id, forest_id) 
+      select userToAdd, tree.forest_id from tree where tree.id = treeid;
+    insert into user_branch(user_id, branch_id) 
+      select userToAdd, branch.id from branch where branch.tree_id = treeid;
+    insert into user_leaf(user_id, leaf_id)
+      select userToAdd, leaf.id from leaf 
+        inner join branch as b
+          on b.tree_id = treeid and
+          leaf.branch_id = b.id
+        where leaf.branch_id = b.id;
+    Commit;
+ELSE
+    select '1';
+END IF;
 END $$
 delimiter ; $$

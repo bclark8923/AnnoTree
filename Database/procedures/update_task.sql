87584db5c2131a58620f2664211959df67c17622
suicidejack <mattprice11@gmail.com>
2013-07-07 21:20:57 -0400

    changed update task

--- Database/procedures/update_task.sql
+++ Database/procedures/update_task.sql
@@ -1,33 +1,62 @@
 -- --------------------------------------------------------------------------------
 -- update_task
--- YEAH BUDDY
+-- On success - returns a 0 if task was updated, 1 if task stayed the same
+-- On error - returns 2 if task does not exist
+--          - returns 3 if user does not have access to tree or tree does not exist
+--          - returns 4 if task status does not exist
+--          - returns 5 if assigned_to user does not exist or have have permissions to tree
+--          - returns 6 if leaf does not exist
 -- --------------------------------------------------------------------------------
 drop procedure if exists `update_task`;
 DELIMITER $$
 
 CREATE procedure `update_task`(
-  in `id` INT,
-  in `description` VARCHAR(1024),
-  in `status` INT,
-  in `leaf_id` INT,
-  in `tree_id` INT,
-  in `assigned_to` INT,
-  in `due_date` TIMESTAMP
-  )
+    in `taskid` INT,
+    in `description` VARCHAR(1024),
+    in `status` INT,
+    in `leafid` INT,
+    in `assignedTo` INT,
+    in `due_date` TIMESTAMP,
+    in requestingUser INT
+)
 BEGIN
-update task as t set
-  t.description = description, 
-  t.status = status, 
-  t.leaf_id = leaf_id, 
-  t.tree_id = tree_id, 
-  t.assigned_to = assigned_to, 
-  t.due_date = due_date  
-where t.id = id;
-if ROW_COUNT() > 0 then 
-select 'id', 'description', 'status', 'leaf_id', 'tree_id', 'assigned_to', 'due_date', 'created_at', 'created_by' 
-union
-select * from task where task.id = id;
-else
-select '1'; 
-end if;
-END
+declare leafcheck int;
+declare usercheck int;
+declare treecheck int;
+select ut.id into treecheck from user_tree ut, task t where ut.user_id = requestingUser and ut.tree_id = t.tree_id and t.id = taskid;
+select u.id into usercheck from user u, user_tree ut where u.id = assignedTo and ut.user_id = u.id and ut.tree_id = treecheck;
+select id into leafcheck from leaf where id = leafid;
+IF (select id from task where id = taskid) THEN
+    IF (treecheck) THEN
+        IF (select id from task_statuses where id = status) THEN
+            IF (assignedTo is null or usercheck) THEN
+                IF (leafid is null or leafcheck) THEN
+                    update task as t set
+                    t.description = description, 
+                    t.status = status, 
+                    t.leaf_id = leafid,  
+                    t.assigned_to = assignedTo, 
+                    t.due_date = due_date  
+                    where t.id = taskid;
+                    IF ROW_COUNT() > 0 THEN
+                        select '0';
+                    ELSE
+                        select '1'; 
+                    END IF;
+                ELSE
+                    select '6';
+                END IF;
+            ELSE
+                select '5';
+            END IF;
+        ELSE 
+            select '4';
+        END IF;
+    ELSE
+        select '3';
+    END IF;
+ELSE
+    select '2';
+END IF;
+END $$ 
+DELIMITER ; $$

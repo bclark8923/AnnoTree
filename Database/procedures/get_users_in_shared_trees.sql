47567e22b41f80d228a1511b2fd35ba67d8a6e64
michael max <mmax@envy>
2013-07-10 22:51:11 -0400

    Updated get_users_in_shared_trees to return email, first_name, last_name

--- Database/procedures/get_users_in_shared_trees.sql
+++ Database/procedures/get_users_in_shared_trees.sql
@@ -7,9 +7,11 @@ CREATE Procedure `get_users_in_shared_trees`(
     in user_in int
 )
 BEGIN
-    select distinct ut2.user_id from user_tree as ut 
+    select distinct u.email, u.first_name, u.last_name from user_tree as ut 
                   inner join user_tree as ut2 
                       on ut.tree_id = ut2.tree_id
+                  inner join user as u on
+                      u.id = ut2.user_id
     where ut.user_id = user_in;
 END $$
 delimiter ; $$

aacea6a098015345ff87bb9c2b0bdf8774f75fda
michael max <mmax@envy>
2013-07-10 22:35:00 -0400

    Added get_users_in_shared_trees

---
+++ Database/procedures/get_users_in_shared_trees.sql
@@ -0,0 +1,17 @@
+use annotree;
+drop  procedure IF EXISTS `get_users_in_shared_trees`;
+DELIMITER $$
+
+
+CREATE Procedure `get_users_in_shared_trees`(
+    in user_in int
+)
+BEGIN
+    select distinct ut2.user_id from tree t 
+                  left join user_tree as ut 
+                      on ut.tree_id = t.id
+                  left join user_tree as ut2 
+                      on ut.tree_id = ut2.tree_id
+    where ut.user_id = user_in;
+END $$
+delimiter ; $$

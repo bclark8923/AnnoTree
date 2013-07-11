2a8f032c48f7b3c9250619089d49d06f6b27b0d6
michael max <mmax@envy>
2013-07-10 22:47:05 -0400

    Changed imput vars to be underscores and not cammel case to follow conventions

--- Database/procedures/add_user_to_tree.sql
+++ Database/procedures/add_user_to_tree.sql
@@ -10,16 +10,16 @@ DELIMITER $$
 
 CREATE Procedure `add_user_to_tree`(
     in treeid int,
-    in userToAdd int,
-    in requestingUser INT
+    in user_to_add int,
+    in requesting_user INT
 )
 BEGIN
-IF (select id from user_tree where tree_id = treeid and user_id = requestingUser) THEN
-    IF (select id from user where id = userToAdd) THEN
+IF (select id from user_tree where tree_id = treeid and user_id = requesting_user) THEN
+    IF (select id from user where id = user_to_add) THEN
         START TRANSACTION;
-        insert into user_tree (user_id, tree_id) values (userToAdd, treeid);
+        insert into user_tree (user_id, tree_id) values (user_to_add, treeid);
         insert into user_forest(user_id, forest_id) 
-          select userToAdd, tree.forest_id from tree where tree.id = treeid;
+          select user_to_add, tree.forest_id from tree where tree.id = treeid;
         Commit;
         select '0';
     ELSE

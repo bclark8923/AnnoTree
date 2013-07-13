4be843a7342ada78af01cda858de7721fd667b71
Lord of the Servers <lordoftheservers@silith.io>
2013-07-13 00:24:00 -0400

    fixed database connection issue with preforking web server - should fix issue with mysql connection dropping

--- Website/anno_tree/lib/AnnoTree/Model/MySQL.pm
+++ Website/anno_tree/lib/AnnoTree/Model/MySQL.pm
@@ -15,7 +15,8 @@ sub init {
     $DB = DBIx::Custom->connect(
         dsn         => $dsn,
         user        => '' . $config->{username},
-        password    => '' . $config->{password}
+        password    => '' . $config->{password},
+        connector   => 1
     ) or warn 'Could not connect to DB';
     
     $DB->{mysql_auto_reconnect} = 1;

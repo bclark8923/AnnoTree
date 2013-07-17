2f09a6dce7d0f7ef587dd1478737b5aefb8fdd0b
Brian <bclark8923@gmail.com>
2013-07-16 21:31:05 -0400

    New ANnoTree logo font and moved Silith.IO website to public

--- Silith.io/Silith.IO-Bootstrap/js/main.js
+++ Silith.io/Silith.IO-Bootstrap/js/main.js
@@ -1,1 +1,3 @@
-
+$("#addEmail").click(function() {
+	$("#formID").submit();
+});
--- Website/anno_tree/public/SilithIO/blog/wp-content/themes/SilithIOBlog/sidebar.php
+++
@@ -1,6 +0,0 @@
-  <div id="sidebarcontent">
-    <h4>Archives</h4>
-     <ul>
-      <?php wp_get_archives('type=monthly'); ?>
-     </ul>
-  </div>

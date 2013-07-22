a943fa70d355f3d3268340eafdf6ed92b2569a50
Brian <bclark8923@gmail.com>
2013-07-22 13:46:59 -0400

    Login Button Fix

--- Website/anno_tree/public/AnnoTreeWebsite/AnnoTreeBlog/header.php
+++ Website/anno_tree/public/AnnoTreeWebsite/AnnoTreeBlog/header.php
@@ -52,7 +52,7 @@
 			<div class="three columns valign">
         <div class="navLinks"><a href="http://annotree.com"><img src="/wp-content/themes/AnnoTreeBlog/img/LogoNameWeb.png"/></a></div>
       </div>
-      <div class="one columns push_four valign">
+      <div class="one columns push_three valign">
         <div class="navLinks"><a href="http://annotree.com" class="skip" gumby-goto="top">Home</a></div>
       </div>
       <div class="one columns valign">

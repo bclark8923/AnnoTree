ad6e4e22ea61188991dc564ebf05ff8347302f29
Lord of the Servers <lordoftheservers@silith.io>
2013-07-25 00:35:45 -0400

    moved beta signup to the front page, removed login button, added silith.io link at the bottom

--- Website/AnnoTreeWebsite/js/main.js
+++ Website/AnnoTreeWebsite/js/main.js
@@ -1,78 +1,94 @@
 // Gumby is ready to go
 Gumby.ready(function() {
-	console.log('Gumby is ready to go...', Gumby.debug());
+    console.log('Gumby is ready to go...', Gumby.debug());
 
-	// placeholder polyfil
-	if(Gumby.isOldie || Gumby.$dom.find('html').hasClass('ie9')) {
-		$('input, textarea').placeholder();
-	}
+    // placeholder polyfil
+    if(Gumby.isOldie || Gumby.$dom.find('html').hasClass('ie9')) {
+        $('input, textarea').placeholder();
+    }
 });
 
 // Oldie document loaded
 Gumby.oldie(function() {
-	console.log("This is an oldie browser...");
+    console.log("This is an oldie browser...");
 });
 
 // Touch devices loaded
 Gumby.touch(function() {
-	console.log("This is a touch enabled device...");
+    console.log("This is a touch enabled device...");
 });
 
 // Document ready
 $(function() {
 
-	$('#submitSignUp').submit(function() {
-	  $("#submitEmail").click();
-	  return false;
-	});
+    $('#submitSignUp').submit(function() {
+      $("#submitEmail").click();
+      return false;
+    });
 
-	var buttonEnabled = true;
-	$("#submitEmail").click(function () {
-		var emailVar = $.trim($("#emailInput").val());
-		if(emailVar.length > 0 && isEmail(emailVar) && buttonEnabled) {
-			buttonEnabled = false;
+    var buttonEnabled = true;
+    $("#submitEmail").click(function () {
+        var emailVar = $.trim($("#emailInput").val());
+        alert('test:' + isEmail(emailVar));
+        if(emailVar.length > 0 && isEmail(emailVar) && buttonEnabled) {
+            buttonEnabled = false;
+        alert('test2');
 
-			$("#emailInput").addClass('disabled');
-			$("#submitEmail").addClass('disabled');
-			$("emailInput").prop('disabled', true);
+            $("#emailInput").addClass('disabled');
+            $("#submitEmail").addClass('disabled');
+            $("emailInput").prop('disabled', true);
 
-			var serializedData = {email: emailVar}
-			// fire off the request to /form.php
-		    var request = $.ajax({
-		        url: "http://annotree.com/betasignup",
-		        type: "post",
-		        data: JSON.stringify(serializedData)
-		    });
+            var serializedData = {email: emailVar}
+            // fire off the request to /form.php
+            var request = $.ajax({
+                url: "http://annotree.com/betasignup", //"http://localhost:3000/betasignup", 
+                type: "post",
+                data: JSON.stringify(serializedData)
+            });
 
-		    // callback handler that will be called on success
-		    request.done(function (response, textStatus, jqXHR){
-		        // log a message to the console
-				$("#validateError").hide();
-				$("#singupAppends").hide();
-				$("#signupInfo").hide();
-				$("#signupThanks").show();
-				buttonEnabled = true;
-				$("#emailInput").removeClass('disabled');
-				$("#submitEmail").removeClass('disabled');
-				$("emailInput").prop('disabled', false);
-		    });
+            // callback handler that will be called on success
+            request.done(function (response, textStatus, jqXHR){
+                // log a message to the console
+                $("#validateError").hide();
+                $("#singupAppends").hide();
+                $("#signupInfo").hide();
+                $("#signupThanks").show();
+                buttonEnabled = true;
+                $("#emailInput").removeClass('disabled');
+                $("#submitEmail").removeClass('disabled');
+                $("emailInput").prop('disabled', false);
+            });
 
-		    // callback handler that will be called on failure
-		    request.fail(function (jqXHR, textStatus, errorThrown){
-				buttonEnabled = true;
-				$("#emailInput").removeClass('disabled');
-				$("#submitEmail").removeClass('disabled');
-				$("emailInput").prop('disabled', false);
-		        // log the error to the console
-		        alert('Sorry, our beta sign up is currently down. Please try again later.')
-		    });
-		} else {
-			$("#validateError").show();
-		}
-	});
+            // callback handler that will be called on failure
+            request.fail(function (jqXHR, textStatus, errorThrown){
+                buttonEnabled = true;
+                $("#emailInput").removeClass('disabled');
+                $("#submitEmail").removeClass('disabled');
+                $("emailInput").prop('disabled', false);
+                // log the error to the console
+                //alert('Sorry, our beta sign up is currently down. Please try again later.')
+                $("#errorText").html('Sorry, our beta sign up is currently down. Please try again in a few minutes or email us at contact@annotree.com.');
+                $("#validateError").show();
+                /*                
+                $("#validateError").hide();
+                $("#singupAppends").hide();
+                $("#signupInfo").hide();
+                $("#signupThanks").show();
+                buttonEnabled = true;
+                $("#emailInput").removeClass('disabled');
+                $("#submitEmail").removeClass('disabled');
+                $("emailInput").prop('disabled', false);
+                */
+            });
+        } else {
+            $("#errorText").html('Please Enter a Valid Email');;
+            $("#validateError").show();
+        }
+    });
 
-	function isEmail(email){
-		return /^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*$/.test(email);	
-	}
+    function isEmail(email){
+        return /[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}/.test(email);
+            // /^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*$/.test(email);    
+    }
 });
 

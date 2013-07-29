1b25d937b87f62257668904bad1c814e388e234e
Brian <bclark8923@gmail.com>
2013-07-29 02:59:47 -0700

    Basic Opacity Transition

--- Website/CCPWires/js/main.js
+++ Website/CCPWires/js/main.js
@@ -1,17 +1,47 @@
 $(document).ready(function() {
-      settingsPane = new SlidingPane({
-        id: 'mobileDashboard',
-        targetId: 'containWrapper',
-        side: 'left',
-        width: 240,
-        duration: 0.75,
-        timingFunction: 'ease',
-        shadowStyle: '0px 0px 0px #000'
-      });
-    
-  $("#paneToggle").click(function() {
-    if($("#mobileDashboard").length) {
-        settingsPane.toggle();
-    }
-  });
+  if(typeof SlidingPane == 'function') {
+    settingsPane = new SlidingPane({
+          id: 'mobileDashboard',
+          targetId: 'containWrapper',
+          side: 'left',
+          width: 240,
+          duration: 0.75,
+          timingFunction: 'ease',
+          shadowStyle: '0px 0px 0px #000'
+        });
+      
+    $("#paneToggle").click(function() {
+      if($("#mobileDashboard").length) {
+          settingsPane.toggle();
+      }
+    });
+  }
+
+  $("#trees").css('opacity', 1);
+  $("#leafs").css('opacity', 1);
+  $("#dashboardWrap").css('opacity', 1);
+  $(".cardLarge").css('opacity', 1);
+
+  $(".treeCard").click(function() {
+    $("#trees").css('opacity', 0);
+    setTimeout(function() {
+      window.location.href = "tree.html";
+    }, 250);
+  })
+
+  $("#leafBack").click(function() {
+    $(".cardLarge").css('opacity', 0);
+    $("#dashboardWrap").css('opacity', 0);
+    setTimeout(function() {
+      window.location.href = "tree.html";
+    }, 250);
+  })
+
+  $(".leafCard").click(function() {
+    $("#leafs").css('opacity', 0);
+    $("#dashboardWrap").css('opacity', 0);
+    setTimeout(function() {
+      window.location.href = "leaf.html";
+    }, 250);
+  })
 });

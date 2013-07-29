68eeddf2127976d0fbf914742510a1f485259be0
Brian <bclark8923@gmail.com>
2013-07-28 17:12:40 -0700

    Social Alignment

--- Website/AnnoTreeWebsite/blog/wp-content/themes/AnnoTreeBlog/index.php
+++ Website/AnnoTreeWebsite/blog/wp-content/themes/AnnoTreeBlog/index.php
@@ -8,9 +8,9 @@
         <h6><?php the_time('F jS, Y') ?></h6>
         <p><?php the_excerpt(__('(more...)')); ?></p>
         <p class="success badge">
-          <?php comments_number( '0', '1', '%' ); ?>a
+          <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?>
         </p>
-        
+        <br/>
         <a href="https://twitter.com/share" class="twitter-share-button" data-via="annotree">Tweet</a>
         <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
         

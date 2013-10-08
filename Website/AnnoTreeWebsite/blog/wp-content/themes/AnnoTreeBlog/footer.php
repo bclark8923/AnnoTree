2f60beb095d049e042176c3f48479249245f6ba3
Brian <bclark8923@gmail.com>
2013-10-08 09:34:09 -0700

    Updated website to be correct version

---
+++ Website/AnnoTreeWebsite/blog/wp-content/themes/AnnoTreeBlog/footer.php
@@ -0,0 +1,36 @@
+  <footer>
+    <div class="container" style="font-size:12px;text-align:center;">
+      <div class="row">
+        <div class="col-sm-4 footerCol">
+          <a href="http://silith.io" target="_blank">
+            <img src="/annotestblog/wp-content/themes/AnnoTreeBlog/img/SilithIO.png" style="height:10px;"/>
+          </a>
+        </div>
+        <div class="col-sm-4 footerCol">
+          Contact@AnnoTree.com
+        </div>
+        <div class="col-sm-4 footerCol">
+          &copy; 2013 Silith.IO, Inc.
+        </div>
+      </div>
+    </div>
+  </footer>
+  
+  <!-- Grab Google CDN's jQuery, fall back to local if offline -->
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+  <script>window.jQuery || document.write('<script src="js/libs/jquery-1.9.1.min.js"><\/script>')</script>
+
+  
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-42541655-1', 'annotree.com');
+    ga('send', 'pageview');
+
+  </script>
+
+  </body>
+</html>
---
+++ Website/AnnoTreeWebsite/blog/wp-content/themes/AnnoTreeBlog/sidebar.php
@@ -0,0 +1,19 @@
+  <div id="sidebarcontent">
+    <h4 style="color:#555;">Categories</h4>
+     <ul>
+      <?php
+		$args = array(
+		  'orderby' => 'name',
+		  'parent' => 0
+		  );
+		$categories = get_categories( $args );
+		foreach ( $categories as $category ) {
+			echo '<a href="' . get_category_link( $category->term_id ) . '">' . $category->name . '</a><br/>';
+		}
+		?>
+     </ul>
+    <h4 style="color:#555;">Archives</h4>
+     <ul>
+      <?php wp_get_archives('type=monthly'); ?>
+     </ul>
+  </div>
